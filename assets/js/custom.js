1de6d74bc601c131e4e2cee00d74de43a97808ba
SnarledOkay <huync0603@gmail.com>
2024-08-01 22:16:36 +0700

    commit to main

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,16 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,// vòng lặp
-        margin :10, // khoảng cách item
-        nav:false, // thanh điều hướng 
-        autoplay: true, //tự động chạy
-        autoplayTimeout: 2000, // thời gian chạy
-        dots: false ,// bỏ dấu chấm số thứ tự
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
         }
-    })
-  });
+    }
+})
