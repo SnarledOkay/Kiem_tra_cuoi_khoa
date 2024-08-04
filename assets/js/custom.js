46197bd75bbd938c703f59f43b8567e244c981fa
SnarledOkay <huync0603@gmail.com>
2024-08-04 12:14:56 +0700

    did some changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,20 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
+    autoplay: true ,
+    dots: false, 
+    autoplay: true,
+    autoplayTimeout: 3000, 
     responsive:{
         0:{
-            items:1
+            items:5
         },
         600:{
-            items:1
+            items:5
         },
         1000:{
-            items:1
+            items:5
         }
     }
 })
