ffb8b237cb43d6b7bac794eb693091e75c7a4607
SnarledOkay <huync0603@gmail.com>
2024-08-09 22:21:56 +0700

    did some change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.brand-of-banner').owlCarousel({
         loop:true,
         margin:44,
         nav:false,
         autoplay: true ,
         dots: false, 
-        autoplayTimeout: 2000, 
-        autoplaySpeed: 2000,
+        autoplayTimeout: 3000, 
+        autoplaySpeed: 3000,
         slideTransition:'linear',
         responsive:{
             0:{
@@ -20,25 +20,20 @@ $(document).ready(function(){
             }
         }
     })
-});
-
-$(document).ready(function(){
     $('.bestseller-items-scroll').owlCarousel({
         loop:false,
         margin:30,
         nav:true,
-        dots: false, 
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:2
+                items:3
             },
             1000:{
-                items:4
+                items:5
             }
         }
     }) ;
 });
-
