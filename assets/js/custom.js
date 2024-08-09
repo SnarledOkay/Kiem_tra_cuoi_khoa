cdc9a455e38d95d7400cc87978f8ec734bce83a7
SnarledOkay <huync0603@gmail.com>
2024-08-09 08:26:56 +0700

    put some change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,43 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    autoplay: true ,
-    dots: false, 
-    autoplay: true,
-    autoplayTimeout: 3000, 
-    responsive:{
-        0:{
-            items:5
-        },
-        600:{
-            items:5
-        },
-        1000:{
-            items:5
+$(document).ready(function(){
+    $('.basic-demo-1').owlCarousel({
+        loop:true,
+        margin:44,
+        nav:false,
+        autoplay: true ,
+        dots: false, 
+        autoplayTimeout: 1000, 
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
         }
-    }
-})
+    })
+    $('.basic-demo-2').owlCarousel({
+        loop:false,
+        margin:30,
+        nav:true,
+        dots: false, 
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    }) ;
+});
+
+// $(document).ready(function(){
+    
+// });
+
